6727c7607db5c727d79ffe77775b6dc0109f3016
christiyan.stoyanov@gmail.com <christiyan.stoyanov@gmail.com>
2017-04-19 01:00:11 +0100

    Automation implemented

--- CyberSecurity.Tests/UnitTest1.cs
+++ CyberSecurity.Tests/UnitTest1.cs
@@ -1,14 +1,64 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Selenium.WebDriver.Equip.Settings;
+using Selenium.WebDriver;
+using OpenQA.Selenium.Chrome;
+using CyberSecurity.Controllers;
+using CyberSecurity.Models;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support.Events;
+using OpenQA.Selenium.PhantomJS;
+using chromypack;
+using Microsoft.VisualStudio.TestTools.UnitTesting.Web;
+
 
 namespace CyberSecurity.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class WebApplication
     {
+        public  ChromeDriver webDriver = new ChromeDriver();
+
+        [TestCategory("Manual QA Front End")]
         [TestMethod]
-        public void TestMethod1()
+        public void NavigateToAssessmentCenter( )
         {
+            webDriver = new ChromeDriver();
+            webDriver.Navigate().GoToUrl("http://localhost/CyberSecurity");
+            webDriver.FindElementByPartialLinkText("Assesment Center").Click();
+        }
+
+        [TestCategory("Manual QA Front End")]
+        [TestMethod]
+        public void OpenWebApplication( )
+        {
+            webDriver = new ChromeDriver();
+            webDriver.Navigate().GoToUrl("http://localhost/CyberSecurity");
+            webDriver.FindElementByPartialLinkText("Home").Click();
+        }
+    }
+
+    [TestClass]
+    public class WebApplicationCore
+    {
+        public ChromeDriver webDriver = new ChromeDriver();
+
+        [TestCategory("UnitTestAgainstCore")]
+        [TestMethod]
+        public void TestHomeController()
+        {
+            QuestionController qController = new QuestionController(); 
+        }
+
+        [TestCategory("UnitTestAgainstCore")]
+        [TestMethod]
+        public void TestBusinessQuestionObjectModel()
+        {
+            QuestionBusinessObject qBusinessObject = new QuestionBusinessObject();
+
+            qBusinessObject.QuestionID = 1;          
         }
     }
 }
